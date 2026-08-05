1aed3b7f470b5f680e8988b430f90c2da46c5498
mohamed-bb-jbf <hdjdhhh71@gmail.com>
2026-08-05 23:51:48 +0100

    Create calculator.c

---
+++ Week0/calculator.c
@@ -0,0 +1,29 @@
+#include <cs50.h>
+
+2 #include <stdio.h>
+
+4 int main(void)
+
+التس
+
+5 {
+
+6
+
+int x = int_get("what's x?");
+
+7
+
+8
+
+9
+
+int y = int_get("what's y?");
+
+int zx + y;
+
+printf("%i\n", z)
+
+10
+
+111
