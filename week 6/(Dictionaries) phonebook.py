71a55ac07a1202c443d715f9f8ddbbf85996a2ca
qlf__90.s <hdjdhhh71@gmail.com>
2026-08-13 22:05:27 +0100

    Implement phonebook dictionary and search feature
    Fix variable names and add phonebook functionality.

---
+++ week 6/(Dictionaries) phonebook.py
@@ -0,0 +1,13 @@
+phonebook = {
+    "David": "617-495-1000",
+    "Brian": "617-495-1001",
+    "John": "617-495-1002"
+}
+
+name = input("Name:")
+
+if name in people:
+  numbers = peopl [Names]
+  print(f"Found: {number}")
+else:
+  print("Not found")
